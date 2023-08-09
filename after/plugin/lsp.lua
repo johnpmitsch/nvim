e77337996071f075222c1f8aaa43c6bfc70a9f9d
John Mitsch <john@quicknode.com>
2023-08-08 22:41:15 -0400

    tweaked coc settings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -42,7 +42,7 @@ keyset("n", "]g", "<Plug>(coc-diagnostic-next)", { silent = true })
 
 -- GoTo code navigation
 keyset("n", "gd", "<Plug>(coc-definition)", { silent = true })
-keyset("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
+keyset("n", "gt", "<Plug>(coc-type-definition)", { silent = true })
 keyset("n", "gi", "<Plug>(coc-implementation)", { silent = true })
 keyset("n", "gr", "<Plug>(coc-references)", { silent = true })
 
