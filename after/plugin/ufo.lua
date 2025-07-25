4d04b332acebb1548bece03edb40cea4bc447535
John Mitsch <john@quicknode.com>
2025-07-25 10:58:14 -0400

    UFO update

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -1,11 +1,11 @@
---vim.o.foldcolumn = "1"
---vim.o.foldlevel = 99
---vim.o.foldlevelstart = 99
---vim.o.foldenable = true
---
---vim.keymap.set("n", "zO", require("ufo").openAllFolds)
---vim.keymap.set("n", "zC", require("ufo").closeAllFolds)
---vim.keymap.set("n", "zo", require("ufo").openFoldsExceptKinds)
---vim.keymap.set("n", "zC", require("ufo").closeFoldsWith)
---
---require("ufo").setup()
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.keymap.set("n", "zO", require("ufo").openAllFolds)
+vim.keymap.set("n", "zC", require("ufo").closeAllFolds)
+vim.keymap.set("n", "zo", require("ufo").openFoldsExceptKinds)
+vim.keymap.set("n", "zC", require("ufo").closeFoldsWith)
+
+require("ufo").setup()
