9b7c6d7f95fc17071f14f5e9f8940970b829ecb9
John Mitsch <john@quicknode.com>
2025-11-25 15:35:10 -0500

    wip

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -1,7 +1,18 @@
-vim.o.foldcolumn = "1"
+--vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.opt.foldmethod = "expr"
+
+-- :h vim.treesitter.foldexpr()
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- ref: https://github.com/neovim/neovim/pull/20750
+vim.opt.foldtext = ""
+vim.opt.fillchars:append("fold: ")
+
+-- Open all folds by default, zm is not available
+vim.opt.foldlevelstart = 99
 
 vim.keymap.set("n", "zO", require("ufo").openAllFolds)
 vim.keymap.set("n", "zC", require("ufo").closeAllFolds)
